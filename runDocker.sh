d4fecdb23334536aeb83fc84c049edb9ecd8c820
EC2 Default User <ec2-user@ip-10-0-0-239.eu-west-1.compute.internal>
2016-11-11 00:03:25 +0000

    The runDocker.sh file can now be used to run the image.

--- runDocker.sh
+++ runDocker.sh
@@ -1,2 +1,2 @@
-docker run docker-java-service java -jar ~/jax-rs-service-template-swarm/target/service-1.0-SNAPSHOT-swarm.jar
+docker run -d docker-java-service:0.1 sh -c "java -jar ~/jax-rs-service-template-swarm/target/service-1.0-SNAPSHOT-swarm.jar"
 
